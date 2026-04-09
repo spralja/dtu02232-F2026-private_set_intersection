614720e26a3500ef1cd1aad428c61d93171672d4
bathtuballigator <rosetimlin22@gmail.com>
2026-04-09 15:36:33 +0200

    add running script

---
+++ scripts/run_locally.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+git switch main
+cargo build --release --bin benchmark
+
+for i in {1..100}
+do
+  target/release/benchmark $i
+done
+
+git switch task4
+cargo build --release --bin benchmark
+
+target/release/benchmark 200
+
+git switch task6
+cargo build --release --bin benchmark
+
+for i in {1..100}
+do
+  target/release/benchmark $i
+done
+
+git switch main
