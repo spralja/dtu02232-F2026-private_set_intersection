5eadabe06cb325ddc856001396c0bb28fd469ab8
Rose <rosetimlin22@gmail.com>
2026-04-09 16:03:41 +0200

    add malicious benchmark

--- scripts/run_locally.sh
+++ scripts/run_locally.sh
@@ -16,9 +16,13 @@ target/release/benchmark 200
 git switch task6
 cargo build --release --bin benchmark
 
-for i in {1..100}
+for i in {1..10}
 do
   target/release/benchmark $i
 done
+
+git switch task6_malicious
+cargo build --release --bin benchmark
+target/release/benchmark 200
 
 git switch main
